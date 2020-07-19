8c4f2456ceb1f97efb2802d70226848e5c2e5af7
ThreeInches <1330782530@qq.com>
2020-07-19 21:50:11 +0800

    minshell.c

--- workspace/zombie/minishell.c
+++ workspace/zombie/minishell.c
@@ -5,9 +5,16 @@
 
 int main()
 {
-    char buf[] = "ls   -a    -l   ";
+    while(1)
+    {   
+    //1.等待标准输入
+    char buf[1024] = { 0 };
+    fgets(buf, 1023, stdin);
+    buf[strlen(buf) - 1] = '\0';
+    printf("buf[%s]\n", buf);
+    //2.参数解析
     char* ptr = buf;
-    char* new_argv[32] = { NULL };
+    char* new_argv[32] = { 0};
     int new_argc = 0;
     while(*ptr != '\0')
     {
@@ -28,5 +35,19 @@ int main()
     {
         printf("new_argv[%d] = %s\n", i, new_argv[i]);
     }
+    //3.创建一个子进程
+    pid_t pid;
+    pid = fork();
+    if(pid == 0)
+    {
+    //4.在子进程中进行程序替换
+        execvp(new_argv[0], new_argv);
+        perror("execvp error");//打印上一次系统调用接口使用的错误原因
+        exit(0);
+    }
+    //5.进程等待
+    wait(NULL);
+    }
+
     return 0;
-}
+    }
