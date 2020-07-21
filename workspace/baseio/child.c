ccb36404436bb34f630ac1e556d104f5182341bd
ThreeInches <1330782530@qq.com>
2020-07-21 19:59:21 +0800

    ln ln -s lib64

---
+++ workspace/baseio/child.c
@@ -0,0 +1,6 @@
+#include "child.h"
+
+int add(int x, int y)
+{
+    return x + y;
+}
---
+++ workspace/baseio/main.c
@@ -0,0 +1,9 @@
+#include "child.h"
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+    printf("%d + %d = %d\n", x, y, add(x, y));
+    return 0;
+}
