d74a6ad607b79589305f98767364a3dd5fb1451e
ThreeInches <1330782530@qq.com>
2020-07-23 17:49:31 +0800

    signal.c

--- workspace/zombie/zombie.c
+++ workspace/zombie/zombie.c
@@ -2,11 +2,21 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void sigcb(int signo)
+{
+    printf("recerve a signal: %d\n", signo);
+}
+
 
 int main()
 {
     pid_t pid;
     pid = fork();
+    
+    signal(SIGCHLD, sigcb);
 
     if(pid == 0)
     {
@@ -14,20 +24,20 @@ int main()
         exit(0);//子进程5秒后退出，父进程什么都不做就会成为僵尸进程
     }
     //wait(NULL);
-    int ret, status;
-    while((ret =waitpid(pid, &status, WNOHANG)) == 0)
-    {
-        printf("子进程退出\n");
-        sleep(1);
-    }
-    if((status & 0x7f) == 0)
-    {
-        printf("status = %d\n", (status >> 8) & 0xff);
-    }
-    if(WIFEXITED(status))
-    {
-        printf("status = %d\n", WEXITSTATUS(status));
-    }
+    //int status;
+    //while((ret =waitpid(pid, &status, WNOHANG)) == 0)
+    //{
+    //    printf("子进程退出\n");
+    //    sleep(1);
+    //}
+    //if((status & 0x7f) == 0)
+    //{
+    //    printf("status = %d\n", (status >> 8) & 0xff);
+    //}
+    //if(WIFEXITED(status))
+    // {
+    //   printf("status = %d\n", WEXITSTATUS(status));
+    //}
     while(1)
     {
         printf("=======\n");
